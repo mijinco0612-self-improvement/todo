c2868ab700f4f094bc61cbdc8cdbfa2f357e155d
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-03-13 14:50:37 +0900

    add sql and repository

---
+++ src/main/java/com/mijinco0612selfimprovement/todo/repository/TodoRepository.java
@@ -0,0 +1,21 @@
+package com.mijinco0612selfimprovement.todo.repository;
+
+import com.mijinco0612selfimprovement.todo.model.Todo;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface TodoRepository {
+
+    void insert(Todo todo);
+
+    Todo select(int id);
+
+    List<Todo> findAll();
+}
+
+
+
